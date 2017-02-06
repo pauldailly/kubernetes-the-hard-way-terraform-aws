5ec583177b1ccc4f9d74e606ae53883bd7af83d2
Paul Dailly <paul.dailly@mttnow.com>
2017-02-06 22:59:06 +0000

    Added VPC, subnets, route tables and security groups

---
+++ securitygroup.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "kubernetes-securitygroup" {
+  vpc_id = "${aws_vpc.kubernetes.id}"
+  name = "kubernetes-securitygroup"
+  description = "security group that allows ssh and all egress traffic"
+
+  tags {
+    Name = "kubernetes-securitygroup"
+  }
+}
+
+resource "aws_security_group_rule" "allow_all" {
+    type = "ingress"
+    from_port = 0
+    to_port = 65535
+    protocol = "-1"
+    security_group_id = "${aws_security_group.kubernetes-securitygroup.id}"
+    cidr_blocks = ["10.240.0.0/16"]
+}
+
+resource "aws_security_group_rule" "allow_ssh" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    security_group_id = "${aws_security_group.kubernetes-securitygroup.id}"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_some_kubernetes_access" {
+    type = "ingress"
+    from_port = 6443
+    to_port = 6443
+    protocol = "tcp"
+    security_group_id = "${aws_security_group.kubernetes-securitygroup.id}"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_access_from_this_security_group" {
+    type = "ingress"
+    from_port = 0
+    to_port = 65535
+    protocol = "-1"
+    security_group_id = "${aws_security_group.kubernetes-securitygroup.id}"
+    source_security_group_id = "${aws_security_group.kubernetes-securitygroup.id}"
+}
---
+++ vpc.tf
@@ -0,0 +1,62 @@
+# Internet VPC
+resource "aws_vpc" "kubernetes" {
+    cidr_block = "10.240.0.0/16"
+    instance_tenancy = "default"
+    enable_dns_support = "true"
+    enable_dns_hostnames = "true"
+    enable_classiclink = "false"
+    tags {
+        Name = "kubernetes"
+    }
+}
+
+resource "aws_vpc_dhcp_options" "kubernetes" {
+    domain_name = "eu-west-1.compute.internal"
+
+    tags {
+        Name = "kubernetes"
+    }
+}
+
+resource "aws_vpc_dhcp_options_association" "dns_resolver" {
+    vpc_id = "${aws_vpc.kubernetes.id}"
+    dhcp_options_id = "${aws_vpc_dhcp_options.kubernetes.id}"
+}
+
+# Subnets
+resource "aws_subnet" "kubernetes-public-1" {
+    vpc_id = "${aws_vpc.kubernetes.id}"
+    cidr_block = "10.240.0.0/16"
+    map_public_ip_on_launch = "true"
+    availability_zone = "eu-west-1a"
+
+    tags {
+        Name = "kubernetes-public-1"
+    }
+}
+
+# Internet GW
+resource "aws_internet_gateway" "kubernetes-gw" {
+    vpc_id = "${aws_vpc.kubernetes.id}"
+
+    tags {
+        Name = "kubernetes"
+    }
+}
+
+resource "aws_route_table" "kubernetes-public" {
+    vpc_id = "${aws_vpc.kubernetes.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.kubernetes-gw.id}"
+    }
+
+    tags {
+        Name = "kubernetes-public-1"
+    }
+}
+
+resource "aws_route_table_association" "kubernetes-public-1" {
+    subnet_id = "${aws_subnet.kubernetes-public-1.id}"
+    route_table_id = "${aws_route_table.kubernetes-public.id}"
+}
