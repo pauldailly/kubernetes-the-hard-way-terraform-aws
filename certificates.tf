1f1d1a557383e9e45203862a7d8c988fb77495d3
Paul Dailly <paul.dailly@mttnow.com>
2017-02-13 21:16:43 +0000

    - Added gitignore - added cert generation using cfssl - added etcd host generation

---
+++ certificates.tf
@@ -0,0 +1,43 @@
+# Generate Certificates
+data "template_file" "certificates" {
+    template = "${file("${path.module}/certs/templates/kubernetes-csr.json.tpl")}"
+    depends_on = ["aws_elb.kubernetes-elb","aws_instance.etcd_cluster_members","aws_instance.kubernetes_controllers","aws_instance.kubernetes_workers"]
+    vars {
+      kubernetes_api_elb_dns_name = "${aws_elb.kubernetes-elb.dns_name}"
+      # TODO: Find out why this IP?
+      kubernetes_cluster_dns = "10.32.0.1"
+
+      # Unfortunately, variables must be primitives, neither lists nor maps
+      etcd0_ip = "${aws_instance.etcd_cluster_members.0.private_ip}"
+      etcd1_ip = "${aws_instance.etcd_cluster_members.1.private_ip}"
+      etcd2_ip = "${aws_instance.etcd_cluster_members.2.private_ip}"
+      controller0_ip = "${aws_instance.kubernetes_controllers.0.private_ip}"
+      controller1_ip = "${aws_instance.kubernetes_controllers.1.private_ip}"
+      controller2_ip = "${aws_instance.kubernetes_controllers.2.private_ip}"
+      worker0_ip = "${aws_instance.kubernetes_workers.0.private_ip}"
+      worker1_ip = "${aws_instance.kubernetes_workers.1.private_ip}"
+      worker2_ip = "${aws_instance.kubernetes_workers.2.private_ip}"
+
+      etcd0_dns = "${aws_instance.etcd_cluster_members.0.private_dns}"
+      etcd1_dns = "${aws_instance.etcd_cluster_members.1.private_dns}"
+      etcd2_dns = "${aws_instance.etcd_cluster_members.2.private_dns}"
+      controller0_dns = "${aws_instance.kubernetes_controllers.0.private_dns}"
+      controller1_dns = "${aws_instance.kubernetes_controllers.1.private_dns}"
+      controller2_dns = "${aws_instance.kubernetes_controllers.2.private_dns}"
+      worker0_dns = "${aws_instance.kubernetes_controllers.0.private_dns}"
+      worker1_dns = "${aws_instance.kubernetes_controllers.1.private_dns}"
+      worker2_dns = "${aws_instance.kubernetes_controllers.2.private_dns}"
+    }
+}
+resource "null_resource" "certificates" {
+  triggers {
+    template_rendered = "${ data.template_file.certificates.rendered }"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${ data.template_file.certificates.rendered }' > certs/generated/kubernetes-csr.json"
+  }
+  provisioner "local-exec" {
+    command = "cd certs/generated; cfssl gencert -initca ../config/ca-csr.json | cfssljson -bare ca; cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=../config/ca-config.json -profile=kubernetes kubernetes-csr.json | cfssljson -bare kubernetes"
+  }
+}
--- instance.tf
+++ instance.tf
@@ -1,17 +1,94 @@
 
 resource "aws_instance" "kubernetes_controllers" {
+
   count=3
+
   ami = "${lookup(var.AMIS, var.AWS_REGION)}"
+
   instance_type = "t2.micro"
+
   key_name = "${aws_key_pair.kubernetes_ec2_key.key_name}"
+
   associate_public_ip_address = "true"
+
   iam_instance_profile="${aws_iam_instance_profile.kubernetes-instance-profile.name}"
+
   vpc_security_group_ids=["${aws_security_group.kubernetes-securitygroup.id}"]
-  private_ip="10.240.0.1${count.index}"
+
+  private_ip="${cidrhost(aws_vpc.kubernetes.cidr_block, 10 + count.index)}"
+
   subnet_id="${aws_subnet.kubernetes-public-1.id}"
+
   source_dest_check="false"
 
   tags {
-      Name = "controller${count.index}"
+      Name = "controller-${count.index}"
   }
+}
+
+resource "aws_instance" "kubernetes_workers" {
+
+  count=3
+
+  ami = "${lookup(var.AMIS, var.AWS_REGION)}"
+
+  instance_type = "t2.micro"
+
+  key_name = "${aws_key_pair.kubernetes_ec2_key.key_name}"
+
+  associate_public_ip_address = "true"
+
+  iam_instance_profile="${aws_iam_instance_profile.kubernetes-instance-profile.name}"
+
+  vpc_security_group_ids=["${aws_security_group.kubernetes-securitygroup.id}"]
+
+  private_ip="${cidrhost(aws_vpc.kubernetes.cidr_block, 20 + count.index)}"
+
+  subnet_id="${aws_subnet.kubernetes-public-1.id}"
+
+  source_dest_check="false"
+
+  tags {
+      Name = "worker-${count.index}"
+  }
+}
+
+resource "aws_instance" "etcd_cluster_members" {
+
+  count=3
+
+  ami = "${lookup(var.AMIS, var.AWS_REGION)}"
+
+  instance_type = "t2.micro"
+
+  key_name = "${aws_key_pair.kubernetes_ec2_key.key_name}"
+
+  associate_public_ip_address = "true"
+
+  iam_instance_profile="${aws_iam_instance_profile.kubernetes-instance-profile.name}"
+
+  vpc_security_group_ids=["${aws_security_group.kubernetes-securitygroup.id}"]
+
+  private_ip="${cidrhost(aws_vpc.kubernetes.cidr_block, 30 + count.index)}"
+
+  subnet_id="${aws_subnet.kubernetes-public-1.id}"
+
+  source_dest_check="false"
+
+  tags {
+      Name = "etcd-${count.index}"
+  }
+}
+
+
+output "controller_addresses" {
+  value = ["Controller IPs: ${aws_instance.kubernetes_controllers.*.private_ip}"]
+}
+
+output "worker_addresses" {
+  value = ["Worker IPs: ${aws_instance.kubernetes_workers.*.private_ip}"]
+}
+
+output "etcd_cluster_addresses" {
+  value = ["Worker IPs: ${aws_instance.etcd_cluster_members.*.private_ip}"]
 }
