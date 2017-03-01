318ab2a8e9aa1ef376f6b7c372c9057127997d5c
Paul Dailly <paul@pauldailly.com>
2017-03-01 09:42:28 +0000

    - Only install flannel on workers - Run flannel roles before kubernetes worker role - Configure docker service to use flannel subnet

--- terraform/certificates.tf
+++ terraform/certificates.tf
@@ -6,7 +6,6 @@ data "template_file" "certificates" {
       kubernetes_api_elb_dns_name = "${aws_elb.kubernetes-elb.dns_name}"
       # TODO: Find out why this IP?
       kubernetes_cluster_dns = "10.32.0.1"
-
       # Unfortunately, variables must be primitives, neither lists nor maps
       etcd0_ip = "${aws_instance.etcd_cluster_members.0.private_ip}"
       etcd1_ip = "${aws_instance.etcd_cluster_members.1.private_ip}"
@@ -17,16 +16,15 @@ data "template_file" "certificates" {
       worker0_ip = "${aws_instance.kubernetes_workers.0.private_ip}"
       worker1_ip = "${aws_instance.kubernetes_workers.1.private_ip}"
       worker2_ip = "${aws_instance.kubernetes_workers.2.private_ip}"
-
       etcd0_dns = "${aws_instance.etcd_cluster_members.0.private_dns}"
       etcd1_dns = "${aws_instance.etcd_cluster_members.1.private_dns}"
       etcd2_dns = "${aws_instance.etcd_cluster_members.2.private_dns}"
       controller0_dns = "${aws_instance.kubernetes_controllers.0.private_dns}"
       controller1_dns = "${aws_instance.kubernetes_controllers.1.private_dns}"
       controller2_dns = "${aws_instance.kubernetes_controllers.2.private_dns}"
-      worker0_dns = "${aws_instance.kubernetes_controllers.0.private_dns}"
-      worker1_dns = "${aws_instance.kubernetes_controllers.1.private_dns}"
-      worker2_dns = "${aws_instance.kubernetes_controllers.2.private_dns}"
+      worker0_dns = "${aws_instance.kubernetes_workers.0.private_dns}"
+      worker1_dns = "${aws_instance.kubernetes_workers.1.private_dns}"
+      worker2_dns = "${aws_instance.kubernetes_workers.2.private_dns}"
     }
 }
 resource "null_resource" "certificates" {
